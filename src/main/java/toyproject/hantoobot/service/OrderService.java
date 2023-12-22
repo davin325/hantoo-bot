36aaf973993b11c32e18976fb57c1dbbd2869eb4
davin325 <davin325@gmail.com>
2023-12-22 09:43:58 +0900

    @Transactional 어노테이션추가

--- src/main/java/toyproject/hantoobot/service/OrderService.java
+++ src/main/java/toyproject/hantoobot/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import toyproject.hantoobot.api.HanTooApi;
 import toyproject.hantoobot.model.dto.BuyMarketOrderDto;
 import toyproject.hantoobot.model.dto.CheckBuyOrderDto;
@@ -18,6 +19,7 @@ import toyproject.hantoobot.repository.StockRepository;
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class OrderService {
 
